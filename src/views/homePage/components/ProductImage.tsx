2129349afc9ca76c296db9fde700ea1d42683342
neutrix-developer-seven <developer.seven@neutrix.co>
2024-11-09 03:12:24 +0600

    scale removed

--- src/views/homePage/components/ProductImage.tsx
+++ src/views/homePage/components/ProductImage.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 export function ProductImage({ src, alt, children }: { src: string; alt: string; children: ReactNode }) {
   return (
     <div className="relative w-full h-64 bg-gray-200 rounded-lg">
-      <Image src={src} alt={alt} layout="fill" objectFit="cover" className="transition-transform duration-300 group-hover:scale-105" />
+      <Image src={src} alt={alt} layout="fill" objectFit="cover" className="transition-transform duration-300" />
       {children}
     </div>
   );
